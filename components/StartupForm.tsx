007c07f88cdcfa57497e9b9c7dd60da3efc7a9db
Agrim Pradhananga <agrimp91@gmail.com>
2024-10-30 12:58:05 +0545

    create ptich action

--- components/StartupForm.tsx
+++ components/StartupForm.tsx
@@ -9,6 +9,7 @@ import { formSchema } from "@/lib/validation"
 import { z } from "zod"
 import { toast, useToast } from "@/hooks/use-toast"
 import { useRouter } from "next/navigation"
+import { createPitch } from "@/lib/actions"
 
 const StartupForm = () => {
   const [errors, setErrors] = useState<Record<string, string>>({})
@@ -26,18 +27,17 @@ const StartupForm = () => {
       }
 
       await formSchema.parseAsync(formValues)
-      console.log(formValues)
 
-      //   const result = await createIdea(prevState,formData,pitch)
+      const result = await createPitch(prevState, formData, pitch)
 
-      //   if (result.status == "SUCCESS") {
-      //     toast({
-      //       title: "Success",
-      //       description: "Your startup pitch has been created succesfully",
-      //     })
-      //     router.push(`/startip/${result.id}`)
-      //   }
-      //   return result
+      if (result.status == "SUCCESS") {
+        toast({
+          title: "Success",
+          description: "Your startup pitch has been created succesfully",
+        })
+        router.push(`/startup/${result._id}`)
+      }
+      return result
     } catch (error) {
       if (error instanceof z.ZodError) {
         const fieldErrors = error.flatten().fieldErrors
